0afba6b86d90defe5f4447087254d8a69e9b3b44
mcrd25 <mayacrd25@gmail.com>
2019-10-09 08:06:03 -0400

    Fixed liniting errors

--- src/components/api/get_weather_data.js
+++ src/components/api/get_weather_data.js
@@ -3,9 +3,9 @@ import apiKey from './api_key';
 const weatherFactory = (city) => {
   const getData = () => {
     fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=${apiKey}&q=${city}&units=imperial`)
-      .then((res) => res.json())
-      .then((data) => data)
-      .catch((err) => alert(err));
+      .then(res => res.json())
+      .then(data => data)
+      .catch(err => alert(err));
   };
 
   return { getData };
