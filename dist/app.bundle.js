dbe4f06f4e7aa8ab9d9cb7d504c6b2c7d2097ac1
mcrd25 <mayacrd25@gmail.com>
2019-10-12 08:11:17 -0400

    WIP: Weather app style and layout

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -104,7 +104,7 @@ eval("exports = module.exports = __webpack_require__(/*! ../../../css-loader/dis
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\")(false);\n// Module\nexports.push([module.i, \".container {\\n  background-color: blue; }\\n\", \"\"]);\n\n\n//# sourceURL=webpack:///./src/style/sass/main.scss?./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js");
+eval("exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\")(false);\n// Module\nexports.push([module.i, \"body {\\n  background-color: #e0f7fa; }\\n\\nnav {\\n  width: 100%; }\\n\\n.card {\\n  width: 50%;\\n  margin: auto !important;\\n  margin-top: 1rem !important; }\\n\", \"\"]);\n\n\n//# sourceURL=webpack:///./src/style/sass/main.scss?./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js");
 
 /***/ }),
 
