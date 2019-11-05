e2da507be35702e8b0caeff2da4ad1b6614d9cea
mcrd25 <mayacrd25@gmail.com>
2019-11-05 08:40:34 -0400

    Unommented commented out code

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import './style/sass/main.scss';
 import 'materialize-css/dist/css/materialize.min.css';
 
-// import getWeatherFactory from './components/api/get_weather_data';
+import getWeatherFactory from './components/api/get_weather_data';
 
-// const cityData = getWeatherFactory();
+const cityData = getWeatherFactory();
 
-// cityData.getData('London');
+cityData.getData('London');
 
-// console.log(data);
+console.log(data);
