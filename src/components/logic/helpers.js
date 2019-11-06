15ad17d3fd4ed90c891a8f8b1e37e74f29278843
mcrd25 <mayacrd25@gmail.com>
2019-11-06 19:28:17 -0400

    Added function to convert date to string and time string

--- src/components/logic/helpers.js
+++ src/components/logic/helpers.js
@@ -1,4 +1,4 @@
-export const getMonth = (date) => {
+const getMonth = (date) => {
 	return date.toLocaleString('default', {
 		month: 'long'
 	});
@@ -8,7 +8,23 @@ export const convertDateTime = (dateInUTC) => {
 	return new Date(dateInUTC * 1000);
 }
 
-export const dayOfWeek = (date) => {
+const dayOfWeek = (date) => {
 	const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 	return days[date.getDay()];
+}
+
+export const getTimeString = (date) => {
+	const hour = date.getHours();
+	if (hour === 0) {
+		return `12:${date.getMinutes()}AM`;
+	}
+	else if (hour < 12) {
+		return `${hour}:${date.getMinutes()}AM`;
+	} else {
+		return `${hour - 12 }:${date.getMinutes()}PM`;
+	}
+}
+
+export const dateTimeString = (date) => {
+	return `${dayOfWeek(date)}, ${getMonth(date)} ${date.getDate()} ${getTimeString(date)}`;
 }
--- src/components/logic/weather_app.js
+++ src/components/logic/weather_app.js
@@ -1,14 +1,17 @@
 import {
-  convertDateTime
+  convertDateTime, dateTimeString, getTimeString
 } from "./helpers";
 const weatherFactory = ({ cod, dt, weather, name, sys, main, wind, visibility}) => {
   if (cod === 200) {
     const { temp, pressure, humidity, temp_min, temp_max } = main;
     // const temp = main.
     const location = `${name}, ${sys.country}`;
-    const sunriseTime = convertDateTime(sys.sunrise);
-    const sunsetTime = convertDateTime(sys.sunset);
-    const date = convertDateTime(dt);
+    const sunriseTimeJS = convertDateTime(sys.sunrise);
+    const sunsetTimeJS = convertDateTime(sys.sunset);
+    const sunrise = getTimeString(sunriseTimeJS);
+    const sunset = getTimeString(sunsetTimeJS);
+    const jsDate = convertDateTime(dt);
+    const date = dateTimeString(jsDate);
     const theme = weather[0].main;
     const description = weather[0].description;
     const weatherIcon = weather[0].icon;
@@ -23,6 +26,8 @@ const weatherFactory = ({ cod, dt, weather, name, sys, main, wind, visibility}) 
       temp_min,
       visibility,
       date,
+      sunrise,
+      sunset,
       windInfo,
       theme,
       description,
